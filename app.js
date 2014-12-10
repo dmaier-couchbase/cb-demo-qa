21d1cac8fd240d97dea021037b89279843d55eee
David Maier <david.maier@couchbase.com>
2014-12-10 22:35:48 +0000

    Added first service method as GET for testing purposes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //Basic express requirements
 var express = require('express');
-var path = require('path');
 var fs = require('fs');
-var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,17 +8,49 @@ var bodyParser = require('body-parser');
 //Couchbase requirements
 var couchbase = require('couchbase');
 
-//Set up logging
-//var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
-//var logger = morgan('combined', {stream: accessLogStream});
-
 //The express application
 var app = express();
 
 //The static resources
-//app.use(logger);
 app.use('/',express.static(__dirname + '/app'));
 app.use('/bower_components',express.static(__dirname + '/bower_components'));
+
+//The service implementation
+var SERVICE_URL = '/service/';
+
+app.get(SERVICE_URL + 'question', function (req, res) {
+	
+    var cust = req.query.cust;
+    var rfp = req.query.rfp;
+    var version = req.query.version;
+    var category = req.query.category;
+    var comment = req.query.comment;
+    var q = req.query.q;
+    var a = req.query.a;
+    
+    if (cust && rfp && version && category && q)
+    {
+        var question = {};
+        question.question = q;
+        question.cust = cust;
+        question.rfp = rfp;
+        question.version = version;
+        question.category = category;
+        
+        if (a) question.answer = a;
+        if (comment) {
+            
+            question.comments = [];
+            question.comments.push(comment);
+        }
+        
+        res.json(question);
+    }
+    else
+    {
+        res.json({"error" : "Could not create the question"});
+    }    
+});
 
 
 //Web server
