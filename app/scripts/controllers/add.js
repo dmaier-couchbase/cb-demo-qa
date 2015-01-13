cd584c471e1d421c16cc171416919b21d69a455e
David Maier <david.maier@couchbase.com>
2015-01-13 10:58:24 +0100

    Added a basic form validation for the Add Question form

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -3,42 +3,64 @@
 var app = angular.module('cbDemoQaApp');
 
 app.controller('AddCtrl', function($scope, QAService) {
-    
-    $scope.action = {};
-    $scope.action.error = "hidden";
-    $scope.action.success = "hidden";
+        
+    //The service can not be injected because the scope can't be passed as parameter to service at construction time otherwise
+    var MsgService = new TMsgService($scope);
+    MsgService.showMsg("none","");
     
     $scope.onAddClicked = function(cust, rfp, version, cat, q, a)
     {        
-        //TQAService.prototype.add = function(customer, rfp, version, category, question, answer)
-        QAService.add(cust, rfp, version, cat, q, a).then(     
-            function(ctx) {            
-                
-                var result = ctx.data;
-                
-                if (result.error)
-                {
-                    $scope.action.error = "";
-                    $scope.action.success = "visibility: hidden";
-                    $scope.action.msg = result.error.msg;
-                    
-                }
-                else
-                {
-                    //Show the success message
-                    $scope.action.error = "visibility: hidden";
-                    $scope.action.success = "";
-                    $scope.action.msg = "Successfully added the question";    
-                }                
+        //Validate
+        var validated = true;
+        var fields = [];
+        
+        if (cust == null || cust == "")
+        {
+            validated = false;
+            fields.push("Customer");
+        }
+        
+        if (rfp == null || rfp == "")
+        {
+            validated = false;
+            fields.push("RFP");
+        }
+        
+        if (q == null || q == "")
+        {
+            validated = false;
+            fields.push("Question");
+        }
+        
+        if (!validated)
+        {
+            MsgService.showMsg("error","The following fields are required" + JSON.stringify(fields));  
+        }
+        else
+        {
+             //TQAService.prototype.add = function(customer, rfp, version, category, question, answer)
+            QAService.add(cust, rfp, version, cat, q, a).then(     
             
-            },
-            function(error) {
+                function(ctx) {            
                 
-                $scope.action.error = "";
-                $scope.action.success = "visibility: hidden";
-                $scope.action.msg = JSON.stringify(error);
-            }                                            
-        ); 
+                    var result = ctx.data;
+                
+                    if (result.error)
+                    {
+                        MsgService.showMsg("error", result.error.msg);                    
+                    }
+                    else
+                    {
+                        //Show the success message
+                        MsgService.showMsg("success", "Successfully added the question");                }                
+            
+                    },
+                    function(error) {
+                
+                        MsgService.showMsg("error", JSON.stringify(error));
+                    }                                            
+            );         
+        }
     }
 });
 
---
+++ app/scripts/services/msgservice.js
@@ -0,0 +1,39 @@
+/**
+ * TMsgService
+ */
+
+/*
+ * Constructor
+ */
+function TMsgService(scope)
+{
+  this.scope = scope; 
+}
+
+/**
+ * To set the model of an action in order to show the success or error message
+ * The idea is to us this service in several controllers, whereby the view imports msg.html
+ */
+TMsgService.prototype.showMsg = function(type, msg)
+{
+        if (type == "none")
+        {
+            this.scope.action = {};
+            this.scope.action.error = "hidden";
+            this.scope.action.success = "hidden";
+        }
+        
+        if (type == "success")
+        {
+            this.scope.action.error = "hidden";
+            this.scope.action.success = "";
+            this.scope.action.msg = msg;
+        }
+        
+        if (type == "error")
+        {
+            this.scope.action.error = "";
+            this.scope.action.success = "hidden";
+            this.scope.action.msg = msg; 
+        }
+}
